7f78750e328d77395a7d6a138c4bd78e48c80d22
Polus Gorgees <polusgorgees123@gmail.com>
2021-10-14 12:29:11 -0700

    4th commit

--- calculator.py
+++ calculator.py
@@ -1,22 +1,50 @@
 def calculator(number1,number2,operator):
     if operator == "+":
-        print(number1 + number2)
+        print(float(number1 + number2))
     elif operator == "-":
-        print(number1 - number2)
+        print(float(number1 - number2))
     elif operator == "*":
-        print(number1 * number2)
+        print(float(number1 * number2))
     elif operator == "/":
-        print(number1 / number2)
+        print(float(number1 / number2))
     elif operator == "//":
-        print(number1 // number2)
+        print(float(number1 // number2))
     elif operator == "**":
-        print(number1 ** number2)
+        print(float(number1 ** number2))
     else:
         return False
 def parse_input():
-    n1 = float
-    n2 = float
-    op = ''
-    #input(f'Enter equation:{n1} {op} {n2}')
-    input(f'Enter equation: %f %s %f'% n1, op, n2)
+    userInput = input(f'Enter equation: ')
+    if userInput.__contains__('+'):
+        x = userInput.split("+")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '+'
+    elif userInput.__contains__('-'):
+        x = userInput.split("-")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '-'
+    elif userInput.__contains__('*'):
+        x = userInput.split("*")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '*'
+    elif userInput.__contains__('/'):
+        x = userInput.split("/")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '/'
+    elif userInput.__contains__('//'):
+        x = userInput.split("//")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '//'
+    elif userInput.__contains__('**'):
+        x = userInput.split("**")
+        n1 = int(x[0])
+        n2 = int(x[1])
+        op = '**'
+    else:
+        print('invalid operation.')
     calculator(n1,n2,op)
