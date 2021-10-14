6c34b0538d12f8d6703cc0c263df78f5457e0346
Polus Gorgees <polusgorgees123@gmail.com>
2021-10-13 20:37:30 -0700

    second commit

---
+++ timing.py
@@ -0,0 +1,10 @@
+from time import time
+#function to gerate execution time
+def calculate_time(func):
+    def time_rn():
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        execution_time = end_time-start_time
+        print(f"EXACTLY  {execution_time} second")
+    return time_rn
