53a88fb97c79b4224c14e3fc0a1fde3278d410f5
Polus Gorgees <polusgorgees123@gmail.com>
2021-10-13 20:59:05 -0700

    third

--- calculator.py
+++ calculator.py
@@ -14,7 +14,8 @@ def calculator(number1,number2,operator):
     else:
         return False
 def parse_input():
-    n1 = float(input("Enter the first number: "))
-    n2 = float(input("Enter the second number: "))
-    op = input("Enter the operator: ")
+    n1 = float
+    n2 = float
+    op = ''
+    input(f'Enter equation:{n1} {op} {n2}')
     calculator(n1,n2,op)
