cb0623061e52553e8ba26543de638aadc6bd20b1
Polus Gorgees <polusgorgees123@gmail.com>
2021-10-14 21:06:41 -0700

    12th commit

--- timing.py
+++ timing.py
@@ -3,7 +3,7 @@ import time
 def calculate_time(func):
     def time_rn():
         start_time = time.time()
-        time.sleep(2)
+        func()
         end_time = time.time()
         execution_time = end_time-start_time
         print("Total time: " + str(execution_time))
